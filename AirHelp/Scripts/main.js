7946d594af45882c94a71fb480d299d1e9f2e5b8
Yordan <Yordan@DESKTOP-DPLCC8R>
2017-12-22 00:43:15 +0200

    dropdown

--- AirHelp/Scripts/main.js
+++ AirHelp/Scripts/main.js
@@ -1,41 +1,56 @@
 ﻿$(document).ready(function () {
-    debugger;
 
     $('[dropdown]').blur(function () {
         var $this = $(this);
         var $dropDown = $this.parent().find('.lSuggestoin');
-        $dropDown.hide();
+        if(($this.parent().find('li:hover').length) == 0)
+        {
+           $dropDown.hide();
+        }
+    });
+
+    $('.lSuggestoin li').click(function () {
+        var $parent = $(this).parent();
+        $parent.parent().find('input').val($(this).text());
+        $parent.hide();
     });
 
     $('[dropdown]').keyup(function (e) {
+        if (e.which == 37 || e.which == 39)
+        {
+            return;
+        }
         var $this = $(this);
         var $dropDown = $this.parent().find('.lSuggestoin');
         if (e.which == 13) {
-            var $selected = $dropDown.first('li.selected');
-            if ($selected.length > 1) {
-                $this.val($selected.text());
+            var $selected = $dropDown.find('li.selected');
+            if ($selected.length > 0) {
+                $this.parent().find('input').val($selected.text());
             }
+            $dropDown.hide();
         }
         // up
         else if (e.which == 38) {
-            var $selected = $dropDown.first('li.selected');
+            var $selected = $dropDown.find('li.selected');
             if (($selected).length == 0) {
-                $dropDown.find('li').last().addClass('selected');
+                $dropDown.find('li[last]').addClass('selected');
             }
-            else if ($selected.prev('li').length > 1) {
+            else if ($selected.attr('index') != '1') {
+                var index = parseInt($selected.attr('index')) - 1
                 $selected.removeClass('selected');
-                $selected.prev('li').addClass('selected');
+                $dropDown.find('[index="' + index + '"]').addClass('selected');
             }
         }
         // down
         else if (e.which == 40) {
-            var $selected = $dropDown.first('li.selected');
+            var $selected = $dropDown.find('li.selected');
             if (($selected).length == 0) {
                 $dropDown.find('li').first().addClass('selected');
             }
-            else if ($selected.next('li').length > 1) {
+            else if ($selected.not('[last]')) {
+                var index = parseInt($selected.attr('index')) + 1
                 $selected.removeClass('selected');
-                $selected.next('li').addClass('selected');
+                $dropDown.find('[index="' + index + '"]').addClass('selected');
             }
         }
         else if ($(this).val().length >= 2) {
