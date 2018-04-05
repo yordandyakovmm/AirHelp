03da5e3cd3d7b17083102ea2c4315559a5144cc4
yordandyakovmm <yordan.dyakov@mentormate.com>
2018-04-05 20:02:46 +0300

    commit

--- AirHelp/Controllers/CommonController.cs
+++ AirHelp/Controllers/CommonController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Net.Mail;
+using System.Web.Mvc;
 
 
 namespace AirHelp.Controllers
@@ -13,6 +15,43 @@ namespace AirHelp.Controllers
         {
             return View("ContactForm");
         }
+
+        [HttpPost]
+        [Route("контакти")]
+        public ActionResult ContactPost()
+        {
+            var email = Request.Form["Email"];
+            var name = Request.Form["name"];
+            var text = Request.Form["Text"];
+
+            string bosy = $"<h1>Запитване</h1><p>Име: {name}</p><p>email: {email}</p><p>----------</p><p>{text}</p>";
+
+
+            MailMessage message = new MailMessage();
+            message.To.Add(new MailAddress("yordan.dyakov@mentormate.com"));
+            message.Subject = "message";
+            message.Body = bosy;
+            message.IsBodyHtml = true;
+
+            try
+            {
+                using (var smtp = new SmtpClient())
+                {
+                    smtp.Port = 25;
+                    smtp.EnableSsl = false;
+                    smtp.Credentials = new System.Net.NetworkCredential("postmaster@helpclaim.eu", "K4hvd2357@");
+                    smtp.Host = "mail.helpclaim.eu";
+                    smtp.Send(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                return View("Success");
+            }
+
+            return View("Success");
+        }
+
 
         [HttpGet]
         [Route("за-нас")]
