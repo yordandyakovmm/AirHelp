bb6656c12c8bce0cde81dde19d4be529edb89907
yordandyakovmm <yordan.dyakov@mentormate.com>
2018-04-10 00:26:27 +0300

    responsive

--- AirHelp/Scripts/Signiture.js
+++ AirHelp/Scripts/Signiture.js
@@ -1,8 +1,134 @@
-﻿
+﻿function initSigniture() {
+
+    //el = document.getElementById('signiture-div');
+    //if (!el) {
+    //    return;
+    //}
+
+    conv = document.getElementById('signiture');
+    ctx = conv.getContext('2d');
+    ctx.strokeStyle = 0;
+    ctx.lineWidth = 3;
+    ctx.lineJoin = 'round';
+    ctx.strokeStyle = '#000000';
+    ctx.shadowColor = '#000000';
+    ctx.shadowBlur = 0.6;
+    isDrawing = false;;
+    couunt = 0;
 
 
-function setContext(ctx, distance, x, y)
-{
+    //conv.ontouchstart = onmousedown;
+    conv.addEventListener("touchmove", onmousemove1, false); 
+    conv.addEventListener("touchend", onmouseup, false);
+
+
+
+
+
+    conv.onmousedown = onmousedown;
+    conv.onmouseup = onmouseup;
+    conv.onmousemove = onmousemove;
+
+    conv.mouseout = function (e) {
+        isDrawing = false;
+
+    };
+
+    conv.mousein = function (e) {
+        debugger;
+        //console.log('in');
+    };
+
+
+
+}
+
+
+function onmousedown(e) {
+    e.preventDefault();
+    isDrawing = true;
+    x = e.offsetX ;
+    y = e.offsetY ;
+    ctx.moveTo(x, y);
+    console.log(x, y);
+    //e.stopPropagation();
+};
+
+
+function onmousemove(e) {
+    e.preventDefault();
+    if (isDrawing) {
+        console.log(x, y);
+        couunt++;
+        var newX = e.offsetX; 
+        var newY = e.offsetY;
+        var distance = (x - newX) ** 2 + (y - newY) ** 2;
+        setContext(ctx, distance, x, y);
+        ctx.lineTo(newX, newY);
+        x = newX;
+        y = newY;
+        ctx.stroke();
+    }
+    if (couunt > 80) {
+        $('.form-box-signiture').removeClass('error').addClass('success');
+        //saveSigiture();
+    }
+};
+
+
+function onmousemove1(e) {
+    e.preventDefault();
+    if (x) {
+        console.log(x, y);
+        couunt++;
+        var rect = this.getBoundingClientRect();
+        var newX = e.touches[0].clientX - rect.left;
+        var newY = e.touches[0].clientY - rect.top;
+        var distance = (x - newX) ** 2 + (y - newY) ** 2;
+        setContext(ctx, distance, x, y);
+        ctx.lineTo(newX, newY);
+        x = newX;
+        y = newY;
+        ctx.stroke();
+    }
+    else {
+        var rect = this.getBoundingClientRect();
+        x = e.touches[0].clientX - rect.left;
+        y = e.touches[0].clientY - rect.top;
+    }
+    if (couunt > 80) {
+        $('.form-box-signiture').removeClass('error').addClass('success');
+        //saveSigiture();
+    }
+};
+
+function onmouseup(e) {
+    x = y = null
+    isDrawing = false;
+    saveSigiture();
+    e.stopPropagation();
+
+};
+
+
+
+function saveSigiture() {
+    var dataURL = conv.toDataURL('image/png');
+    $('[name=SignitureImage]').val(dataURL);
+}
+
+function clearSignature() {
+    conv.width = conv.width;
+    $('.form-box-signiture').removeClass('success').removeClass('error');
+    //var html = $('.form-box-signiture > div.convas-holder').html();
+    //$('.form-box-signiture > div.convas-holder').html('');
+    //$('.form-box-signiture > div.convas-holder').html(html);
+    initSigniture();
+
+}
+
+
+function setContext(ctx, distance, x, y) {
     if (distance < 50 && type != 0) {
         type = 0;
         ctx.beginPath();
--- AirHelp/Scripts/main.js
+++ AirHelp/Scripts/main.js
@@ -7,16 +7,6 @@
         }
     });
 
-    //nitialize width of position absolute elements
-    //$('.lSuggestoin').each(function (el) {
-    //    $(this).width($(this).parent().find('input').width() + 50);
-    //});
-    //$(window).resize(function () {
-    //    $('.lSuggestoin').each(function (el) {
-    //        $(this).width($(this).parent().find('input').width() + 50);
-    //    });
-    //});
-
     $.datepicker.setDefaults($.datepicker.regional['bg']);
 
     $('#Date').datepicker({ dateFormat: 'dd.mm.yy' });
@@ -69,12 +59,11 @@
 
     // convace 
 
-
-    initSigniture();
+    if (typeof (initSigniture) != 'undefined') {
+        initSigniture();
+    }
 
 });
-
-
 
 
 
@@ -84,83 +73,13 @@ var ctx;
 var isDrawing;
 var couunt = 0;
 var type = 0;
-var x = 0;
-var y = 0;
+var x = null;
+var y = null;
 
 
 
 
-function initSigniture() {
 
-    el = document.getElementById('signiture-div');
-    if (!el)
-    {
-        return;
-    }
-
-    conv = document.getElementById('signiture');
-    ctx = conv.getContext('2d');
-    ctx.strokeStyle = 0;
-    ctx.lineWidth = 3;
-    ctx.lineJoin = 'round';
-    ctx.strokeStyle = '#000000';
-    ctx.shadowColor = '#000000';
-    ctx.shadowBlur = 0.6;
-    isDrawing = false;;
-    couunt = 0;
-
-    el.onmousedown = function (e) {
-        isDrawing = true;
-        x = e.offsetX;
-        y = e.offsetY;
-        ctx.moveTo(e.offsetX, e.offsetX);
-    };
-    
-    el.onmousemove = function (e) {
-        if (isDrawing) {
-            couunt++;
-            var distance = (x - e.offsetX) ** 2 + (y - e.offsetY) ** 2;
-            setContext(ctx, distance, x, y)
-            ctx.lineTo(e.offsetX, e.offsetY);
-            x = e.offsetX;
-            y = e.offsetY;
-            ctx.stroke();
-        }
-        if (couunt > 80) {
-            $('.form-box-signiture').removeClass('error').addClass('success');
-            saveSigiture();
-        }
-    };
-    el.mouseout = function (e) {
-        isDrawing = false;
-        
-    };
-
-    el.mousein = function (e) {
-        debugger;
-        //console.log('in');
-    };
-
-    el.onmouseup = function () {
-        isDrawing = false;
-        //console.log('up');
-        saveSigiture();
-    };
-}
-
-function saveSigiture() {
-    var dataURL = conv.toDataURL('image/png');
-    $('[name=SignitureImage]').val(dataURL);
-}
-
-function clearSignature() {
-    $('.form-box-signiture').removeClass('success').removeClass('error');
-    var html = $('.form-box-signiture > div.convas-holder').html();
-    $('.form-box-signiture > div.convas-holder').html('');
-    $('.form-box-signiture > div.convas-holder').html(html);
-    initSigniture();
-
-}
 
 function addCbAiports(addButton) {
     var $this = $(addButton);
