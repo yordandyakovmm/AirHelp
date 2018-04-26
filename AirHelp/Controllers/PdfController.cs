951ccab58d56c013dbde5c43f6f5a8460f4d3e71
yordandyakovmm <yordan.dyakov@mentormate.com>
2018-04-27 01:26:34 +0300

    atorny

--- AirHelp/Controllers/PdfController.cs
+++ AirHelp/Controllers/PdfController.cs
@@ -28,23 +28,23 @@ namespace AirHelp.Controllers
     public class PdfController : BaseController
     {
 
-        [Route("attorney/{id}")]
-        public ActionResult AttorneyHtml(Guid id)
-        {
-            Claim claim = null;
+        //[Route("attorney/{id}")]
+        //public ActionResult AttorneyHtml(Guid id)
+        //{
+        //    Claim claim = null;
 
-            using (AirHelpDBContext dc = new AirHelpDBContext())
-            {
-                claim = dc.Claims.Include("User").Where(c => c.ClaimId == id).SingleOrDefault();
-            }
+        //    using (AirHelpDBContext dc = new AirHelpDBContext())
+        //    {
+        //        claim = dc.Claims.Include("User").Where(c => c.ClaimId == id).SingleOrDefault();
+        //    }
 
-            var model = new VMClaim(claim);
+        //    var model = new VMClaim(claim);
 
-            return PartialView("Attorney", model);
-        }
+        //    return PartialView("Attorney", model);
+        //}
 
         [Route("attorneyPdf/{id}")]
-        public ActionResult AttorneyHtml2(Guid id)
+        public ActionResult AttorneyHtml(Guid id)
         {
 
             Claim claim = null;
@@ -84,7 +84,61 @@ namespace AirHelp.Controllers
             return null;
         }
 
+        //[Route("contract/{id}")]
+        //public ActionResult ContractHtml(Guid id)
+        //{
+        //    Claim claim = null;
 
+        //    using (AirHelpDBContext dc = new AirHelpDBContext())
+        //    {
+        //        claim = dc.Claims.Include("User").Where(c => c.ClaimId == id).SingleOrDefault();
+        //    }
+
+        //    var model = new VMClaim(claim);
+
+        //    return PartialView("Contract", model);
+        //}
+
+        [Route("contractPdf/{id}")]
+        public ActionResult ContractHtml(Guid id)
+        {
+
+            Claim claim = null;
+
+            using (AirHelpDBContext dc = new AirHelpDBContext())
+            {
+                claim = dc.Claims.Include("User").Where(c => c.ClaimId == id).SingleOrDefault();
+            }
+
+            var model = new VMClaim(claim);
+
+            return PartialView("ContractPdf", model);
+
+        }
+
+        [Route("договор/{id}")]
+        public ActionResult ContractPdf(Guid id)
+        {
+            string port = Request.Url.Port == 80 ? string.Empty : $":{Request.Url.Port.ToString()}";
+
+            String url = $"{Request.Url.Scheme}://{Request.Url.Host}{port}/contractPdf/{id}";
+
+            SelectPdf.HtmlToPdf converter = new SelectPdf.HtmlToPdf();
+
+            converter.Options.MarginTop = 30;
+            converter.Options.MarginBottom = 10;
+            converter.Options.MarginLeft = 20;
+            converter.Options.MarginRight = 20;
+            converter.Options.PdfPageSize = SelectPdf.PdfPageSize.A4;
+
+            SelectPdf.PdfDocument doc = converter.ConvertUrl(url);
+
+            Response.ContentType = "application/pdf";
+            doc.Save(Response.OutputStream);
+            doc.Close();
+            Response.End();
+            return null;
+        }
 
     }
 }
